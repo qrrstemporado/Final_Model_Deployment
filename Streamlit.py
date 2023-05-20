c6d989cfaa7f83a5f5bd089ebb5efa7910204181
qrrstemporado <112641711+qrrstemporado@users.noreply.github.com>
2023-05-20 12:14:41 +0800

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,35 +1,41 @@
 import streamlit as st
 import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('best_model.h5')
-  return model
-model=load_model()
-st.write("""
-# Fashion Mnist Classificator"""
-)
-file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
+    model = tf.keras.models.load_model('best_model.h5')
+    return model
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+def import_and_predict(image_data, model):
+    size = (28, 28)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = image.convert('L')  # Convert image to grayscale
+    img = np.asarray(image)
+    img = img.reshape(1, 28, 28, 1)  # Reshape to match the model's input shape
+    img = img / 255.0  # Normalize pixel values to range [0, 1]
+    prediction = model.predict(img)
     return prediction
+
+model = load_model()
+
+st.write("""
+# Fashion Mnist Classificator
+""")
+
+file = st.file_uploader("Choose plant photo from computer", type=["jpg", "png"])
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
     
-    class_names=['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
                
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    string = "OUTPUT: " + class_names[np.argmax(prediction)]
     st.success(string)
