202d18074c66963f9c268637158dc94fb1e0c3b3
qrrstemporado <112641711+qrrstemporado@users.noreply.github.com>
2023-05-20 12:04:39 +0800

    Create Streamlit.py

---
+++ Streamlit.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('best_model.h5')
+  return model
+model=load_model()
+st.write("""
+# Fashion Mnist Classificator"""
+)
+file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    
+    class_names=[''T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+               
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
